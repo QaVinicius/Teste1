46b40ff4ae130eca6106ddf2fe6ac6f96622715f
Vinicius Santos <viniciusdss17@gmail.com>
2022-04-06 02:27:08 -0300

    primeiro commit

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Vem pra cá (pra Fortal City)\n" +
+                "Ampliar (bang bom)\n" +
+                "A visão\n" +
+                "Eu sou lenda tipo Don, man\n" +
+                "Saí de Fortal, fui lá fazer meu próprio nome\n" +
+                "E hoje eu fiz uma pilha, dá pra escalar\n" +
+                "Quem não gostou, agora vai ter que aturar\n" +
+                "The gun is on the table, quer aprender inglês? (Uh, woah)\n" +
+                "Ela vem treinar com uma aula por mês (uh, woah)\n" +
+                "É só tome, tome, tome, bebe, bebe, tome, tome\n" +
+                "Só sai do forró quando quiser\n" +
+                "The drug is on the table, quer aprender inglês?");
+    }
+}
